968a3d83abebe28a8150196dfa38ff56b363deb3
DubeiA <105717456+DubeiA@users.noreply.github.com>
2023-08-19 07:28:10 +0000

    Deploying to gh-pages from @ DubeiA/Digital-Project@63cb697d5660c4e9f7fcee4d055d11fcc9e894b6 🚀

---
+++ main.js
@@ -0,0 +1,89 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/image/Rectangle.jpg
+const Rectangle_namespaceObject = __webpack_require__.p + "image/Rectangle.jpg";
+;// CONCATENATED MODULE: ./src/image/image 12.png
+const image_12_namespaceObject = __webpack_require__.p + "image/image 12.png";
+;// CONCATENATED MODULE: ./src/image/image 15.png
+const image_15_namespaceObject = __webpack_require__.p + "image/image 15.png";
+;// CONCATENATED MODULE: ./src/image/image 16.png
+const image_16_namespaceObject = __webpack_require__.p + "image/image 16.png";
+;// CONCATENATED MODULE: ./src/image/image 17.png
+const image_17_namespaceObject = __webpack_require__.p + "image/image 17.png";
+;// CONCATENATED MODULE: ./src/image/image 18.png
+const image_18_namespaceObject = __webpack_require__.p + "image/image 18.png";
+;// CONCATENATED MODULE: ./src/image/Rectangle 10.png
+const Rectangle_10_namespaceObject = __webpack_require__.p + "image/Rectangle 10.png";
+;// CONCATENATED MODULE: ./src/image/Rectangle 12.png
+const Rectangle_12_namespaceObject = __webpack_require__.p + "image/Rectangle 12.png";
+;// CONCATENATED MODULE: ./src/image/Rectangle 8.png
+const Rectangle_8_namespaceObject = __webpack_require__.p + "image/Rectangle 8.png";
+;// CONCATENATED MODULE: ./src/image/Rectangle 9.png
+const Rectangle_9_namespaceObject = __webpack_require__.p + "image/Rectangle 9.png";
+;// CONCATENATED MODULE: ./src/image/Group.svg
+const Group_namespaceObject = __webpack_require__.p + "image/Group.svg";
+;// CONCATENATED MODULE: ./src/image/Group 1.svg
+const Group_1_namespaceObject = __webpack_require__.p + "image/Group 1.svg";
+;// CONCATENATED MODULE: ./src/image/symbol-defs.svg
+const symbol_defs_namespaceObject = __webpack_require__.p + "image/symbol-defs.svg";
+;// CONCATENATED MODULE: ./src/js/importImage.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+;// CONCATENATED MODULE: ./src/index.js
+
+
+/******/ })()
+;
